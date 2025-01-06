054fbc3394c242c075e76e9544412656a02c44a0
Jason <153016092+KunhoPark-Jason@users.noreply.github.com>
2025-01-06 15:46:37 +0100

    Rebalancing

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,12 +5,18 @@ public class Weapon : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 10f;
     public float damage = 1f;
+
+    private float maxY = 5.5f;
     void Start()
     {
-        Destroy(gameObject, 0.9f);
+
     }
     void Update()
     {
         transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+        if (transform.position.y > maxY){
+            Destroy(gameObject);
+        }
     }
 }
